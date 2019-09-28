e051809dca6b02a15edc1830fef448fe5c397782
htkachuk <annakristina50@gmail.com>
2019-09-28 16:30:30 +0300

    update route

--- lib/modules/route.dart
+++ lib/modules/route.dart
@@ -16,10 +16,16 @@ class Route implements ListItem {
 
   static List<Route> routes = [
     Route(
-      title: 'Route 1',
-      description: 'Route description',
-      duration: '3 hours',
+      title: 'Слідами Франції',
+      description: 'Це унікальна екскурсія в якій Вам не потрібен екскурсовод! Прогулянка по архітектурним пам\'яткам в супроводі електронного гіда і нової квест-програми. Ніяких нудних монологів, тільки інтерактив, соковиті кадри і приємні бонуси. Екскурсія познайомить Вас з цікавими історіями і міськими легендами, а також дозволить заробити бали для програми лояльності \"Франція в серці\" *. Отже, вперед до знижок і вражень!',
+      duration: 'Тривалість 3 години',
       places: Place.places,
+    ),
+    Route(
+      title: 'Un air de France',
+      description: 'C\'est un tour unique dans lequel vous n\'avez pas besoin d\'un guide! Une promenade à travers les monuments architecturaux, accompagnée d\'un guide électronique et d\'un nouveau questionnaire. Pas de monologues ennuyeux, seulement l\’interactif, des lieux splendides à prendre en photo et des сadeaux sympas. La visite vous fera découvrir des histoires intéressantes et des légendes urbaines, ainsi que gagner des points pour le programme de récompense «France au cœur» *. Alors, allez à des réductions et des impressions!',
+      duration: 'La durée 3 heures',
+      places: Place.placesFr,
     ),
   ];
 }
