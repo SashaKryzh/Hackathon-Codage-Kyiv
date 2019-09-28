456ac5bdbab30b2e0d7e35c8b31026ca97722601
SashaKryzh <sashakryzh@gmail.com>
2019-09-28 11:28:01 +0300

    place class changes

--- lib/modules/place.dart
+++ lib/modules/place.dart
@@ -3,22 +3,25 @@ import 'package:hackathon/modules/list_item.dart';
 class Place implements ListItem {
   String title;
   String description;
+  String shortDescription;
+  String address; //
+  String creationDate;
+  String creator;
   double lat;
   double lng;
 
   Place({
     this.title,
     this.description,
+    this.shortDescription,
+    this.address,
+    this.creationDate,
+    this.creator,
     this.lat,
     this.lng,
-  });
+  })  : assert(title != null),
+        assert(shortDescription != null),
+        assert(address != null);
 
-  static List<Place> places = [
-    Place(
-      title: 'Place',
-      description: 'Description',
-      lat: 46.45209,
-      lng: 30.7337006,
-    )
-  ];
+  static List<Place> places = [];
 }
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,12 +3,38 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    Widget blockTitle(String title) {
+      return Column(
+
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {},
+          )
+        ],
       ),
-      body: Container(
-        color: Colors.green,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 150,
+            ),
+            Text(
+              'Аннушка',
+              style: theme.textTheme.title,
+            ),
+            blockTitle('Achivments'),
+          ],
+        ),
       ),
     );
   }
