a5478bc8137f9a8df0b224f19088ab32ff4804c3
htkachuk <annakristina50@gmail.com>
2019-09-28 12:15:35 +0300

    duration added

--- lib/modules/route.dart
+++ lib/modules/route.dart
@@ -4,11 +4,13 @@ import 'package:hackathon/modules/place.dart';
 class Route implements ListItem {
   String title;
   String description;
+  String duration;
   List<Place> places;
 
   Route({
     this.title,
     this.description,
+    this.duration,
     this.places,
   });
 
@@ -16,7 +18,7 @@ class Route implements ListItem {
     Route(
       title: 'Route 1',
       description: 'Route description',
+      duration: '3 hours',
       places: Place.places,
     ),
   ];
-}
